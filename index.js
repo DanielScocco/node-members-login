b03c1bdea93eb21f0e8c0e7a0d89e7f5874c8a7f
Daniel Scocco <danielscocco@gmail.com>
2016-11-11 16:54:07 -0200

    prior to DB integration

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 "use strict";
 
 var express = require('express');
-var app = express(); 
+var app = express();
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
 
 app.set('view engine','pug');
 app.set('views','./views');
 app.locals.pretty = true;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/signup',function(req,res){
+	console.log(req.body);
+	res.render('index');
+});
 
 app.get('/',function(req,res) {
-    var data = {title:'hello my friend',message:'sup everyone'};
-	res.render('index',data);
+	res.render('index');
+});
+
+app.get('/members',function(req,res){
+	res.render('members');
+});
+
+app.get('*',function(req,res){
+	res.status(404);
+	res.render('404');
 });
 
 app.listen(process.env.PORT || 3000,function(){
